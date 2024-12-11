920f7b3f4b960174446e904489d2d0de3eb532c5
ManojaD2004 <manojadkc2004@gmail.com>
2024-12-12 04:07:02 +0530

    Start

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const chalk = require("chalk");
 
-const app = express();
+async function main() {
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+  dotenv.config({ path: "./.env.local" });
 
-dotenv.config({ path: "./.env.local" });
+  const { PORT } = process.env;
 
-const { PORT } = process.env;
+  app.get("/hello", (req, res) => {
+    try {
+      console.log(chalk.red("HIT /hello"));
+      res.status(200).send("Hello Tiger!");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  });
 
-app.get("/hello", (req, res) => {
-  try {
-    res.status(200).send("Hello Tiger!");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on Port: ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`);
+  });
+}
+main();
